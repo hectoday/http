c066eb4b4c6c2210d0765da23e454896231a6f57
bastianplsfix <sebastian.nomme@icloud.com>
2026-01-12 18:25:32 +0100

    added deno plugin to docs

--- apps/docs/astro.config.ts
+++ apps/docs/astro.config.ts
@@ -1,12 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
-
 import react from "@astrojs/react";
+import deno from "@deno/astro-adapter";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: deno(),
   vite: {
     server: {
       fs: {
